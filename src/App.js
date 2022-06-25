5349ee1c9b4a030af2147fd61114e87a752a48b7
Ahmed-Silat <ahmedsilat95@gmail.com>
2022-06-25 23:25:56 +0500

    quiz-app

--- src/App.js
+++ src/App.js
@@ -1,68 +1,65 @@
 import logo from "./logo.svg";
 import { useState } from "react";
+import list from "./data";
 import "./App.css";
 
 function App() {
   const [index, setIndex] = useState(0);
-  const [text, setText] = useState([]);
-  const [button, setButton] = useState(false);
+  const [quizEnded, setQuizEnded] = useState(false);
+  const [startQuiz, setStartQuiz] = useState(false);
 
-  const list = [
-    {
-      question: "Which one is the smallest ocean in the world ?",
-      options: ["Indian", "Pacific", "Atlantic", "Arctic"],
-    },
-    {
-      question: "Which country gifted the Statue of Liberty to USA in 1886 ?",
-      options: ["France", "Canada", "Brazil", "England"],
-    },
-  ];
-
-  const startQuiz = () => {
-    setButton(true);
+  const onClickStartQuiz = () => {
+    setStartQuiz(true);
   };
 
   const nextQuestion = () => {
-    setIndex(+1);
+    if (index === list.length - 1) {
+      setQuizEnded(true);
+      setIndex(0);
+    } else {
+      setIndex(index + 1);
+    }
   };
   return (
     <div className="App">
       <header className="App-header">
-        {button ? (
+        <h2>Quiz App</h2>
+        {quizEnded ? (
           <div>
-            <h3>{list[index].question}</h3>
-            {list[index].options.map((item) => {
-              return (
-                <div key={item}>
-                  <input key={item} type="radio" value={item} name="ocean" />
-                  {item}
-                </div>
-              );
-            })}
-            <button onClick={nextQuestion}>Next</button>
+            <h3>Quiz Score</h3>
+            <h4>You got 50%</h4>
+            <button onClick={() => setQuizEnded(false)}>Restart Quiz</button>
           </div>
         ) : (
-          <button onClick={startQuiz}>Start Quiz</button>
+          <div>
+            {startQuiz ? (
+              <div>
+                <h3>
+                  {"Q"}
+                  {index + 1}
+                  {") "}
+                  {list[index].question}
+                </h3>
+                {list[index].options.map((item) => {
+                  return (
+                    <div key={item}>
+                      <input
+                        key={item}
+                        type="radio"
+                        value={item}
+                        name="ocean"
+                      />
+                      {item}
+                    </div>
+                  );
+                })}
+                <button onClick={nextQuestion}>Next</button>
+              </div>
+            ) : (
+              <button onClick={onClickStartQuiz}>Start Quiz</button>
+            )}
+          </div>
         )}
-        {/* {button ? (
-          <div>
-            {list[index].map((item) => {
-              // console.log(item)
-              return (
-                <>
-                  <h3>{item[index].question}</h3>
-                  {item[index].options.map((info) => {
-                    // console.log(info)
-                    return <li>{info}</li>;
-                  })}
-                </>
-              );
-            })}{" "}
-            <button onClick={nextQuestion}>Next</button>{" "}
-          </div>
-        ) : (
-          <button onClick={startQuiz}>Start Quiz</button>
-        )} */}
       </header>
     </div>
   );
---
+++ src/data.js
@@ -0,0 +1,58 @@
+const list = [
+  {
+    question: "An HTML document can contain _____",
+    options: ["Attributes", "Tags", "Raw text", "All the answers are true"],
+  },
+  {
+    question: "A page designed in HTML is called _____",
+    options: ["Application", "Cover page", "Front-end", "Web Page"],
+  },
+  {
+    question: "The HTML document contains a root tag called ____",
+    options: ["HEAD", "Title", "Body", "HTML"],
+  },
+  {
+    question:
+      "If we want to place text around an image, which CSS property should we use?",
+    options: ["push", "float", "align", "wrap"],
+  },
+  {
+    question:
+      "Can we align an element by setting margin-left and margin-right?",
+    options: ["Yes its possible.", "No, its not possible."],
+  },
+  {
+    question: "Choose the correct HTML tag for a large title.",
+    options: ["H1", "Heading", "Head", "H6"],
+  },
+  {
+    question:
+      "If we want to use a nice green dotted border around an image, which css property are we going to use?",
+    options: [
+      "border-line",
+      "border-style",
+      "border-decoration",
+      "border-color",
+    ],
+  },
+  {
+    question:
+      "Which of the following keywords is used to define a variable in Javascript?",
+    options: ["var", "let", "Both A and B", "None of the above"],
+  },
+  {
+    question:
+      "Which of the following methods is used to access HTML elements using Javascript?",
+    options: [
+      "getElementById()",
+      "getElementByClassName()",
+      "Both A and B",
+      "None of the above",
+    ],
+  },
+  {
+    question: "How can a datatype be declared to be a constant type?",
+    options: ["const", "var", "let", "constant"],
+  },
+];
+export default list;
