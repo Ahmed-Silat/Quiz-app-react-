504d25e936411d1042e4ba232d15a4fd36dbae85
Ahmed-Silat <ahmedsilat95@gmail.com>
2022-06-25 13:39:09 +0500

    quiz-app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [index, setIndex] = useState(0);
+  const [text, setText] = useState([]);
+  const [button, setButton] = useState(false);
+
+  const list = [
+    {
+      question: "Which one is the smallest ocean in the world ?",
+      options: ["Indian", "Pacific", "Atlantic", "Arctic"],
+    },
+    {
+      question: "Which country gifted the Statue of Liberty to USA in 1886 ?",
+      options: ["France", "Canada", "Brazil", "England"],
+    },
+  ];
+
+  const startQuiz = () => {
+    setButton(true);
+  };
+
+  const nextQuestion = () => {
+    setIndex(+1);
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {button ? (
+          <div>
+            <h3>{list[index].question}</h3>
+            {list[index].options.map((item) => {
+              return (
+                <div key={item}>
+                  <input key={item} type="radio" value={item} name="ocean" />
+                  {item}
+                </div>
+              );
+            })}
+            <button onClick={nextQuestion}>Next</button>
+          </div>
+        ) : (
+          <button onClick={startQuiz}>Start Quiz</button>
+        )}
+        {/* {button ? (
+          <div>
+            {list[index].map((item) => {
+              // console.log(item)
+              return (
+                <>
+                  <h3>{item[index].question}</h3>
+                  {item[index].options.map((info) => {
+                    // console.log(info)
+                    return <li>{info}</li>;
+                  })}
+                </>
+              );
+            })}{" "}
+            <button onClick={nextQuestion}>Next</button>{" "}
+          </div>
+        ) : (
+          <button onClick={startQuiz}>Start Quiz</button>
+        )} */}
       </header>
     </div>
   );
